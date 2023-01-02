4e2977b39b03038aa7de4b0c0ad719c47250da42
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-01-02 14:30:07 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-
+/*
 const gallery = document.querySelector('.gallery')
 const items = []
 
@@ -45,3 +45,52 @@ gallery.addEventListener('click', e => {
 		}
 	})
 })
+*/
+
+const paletteContainer = document.querySelector('.js-palette');
+const cardsMarcup = createGalleryCards(galleryItems)
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarcup);
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+
+function createGalleryCards (galleryItems){
+return galleryItems.map(({preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+})
+.join("")
+
+}
+function onPaletteContainerClick (e){
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+
+    
+    paletteContainer.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+}
